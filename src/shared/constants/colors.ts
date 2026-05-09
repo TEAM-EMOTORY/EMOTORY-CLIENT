439727dfb3c675c984b13bfabec2a7dba763c5d8
2nh2oo <p03je8884@gmail.com>
2026-05-09 10:21:26 +0900

    feat: 컬러 및 폰트 추가

---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  white: '#FFFFFF',
+  background: '#F8FAF9',
+  orange: '#F5622E',
+  primary100: '#FFF9F0',
+  primary200: '#FEF1D3',
+  primary300: '#F8E5CD',
+  primary500: '#FCC944',
+  navy100: '#1C599D',
+  navy200: '#0F154D',
+  brown100: '#CCBBA6',
+  brown200: '#68462B',
+} as const
