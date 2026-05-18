b2d9c33cda6f62f43a16f0655a408f42f8e85a51
2nh2oo <p03jeun@gmail.com>
2026-05-18 14:06:50 +0900

    Feat: input 컴포넌트 (#15)
    * fix: Vite 환경 변수 설정 및 CSS 타입 선언 추가
    
    * feat: 컬러 및 폰트 추가
    
    * style: 텍스트 세부 설정
    
    * fix: 폰트 이름 통일
    
    * style: 디자인 (폰트, 색상) 설정 및 가이드 추가
    
    * refactor: 폰트 상속 적용, 배경색 삭제
    
    * refactor: 컬러 수정 및 폰트 로드
    
    * refactor:최최종
    
    * fix: 바엑 패키지 설정 및 폴더 분리
    
    * fix: 티머니 폰트 적용
    
    * fix: 바엑래시피 설치
    
    * feat: 인풋 컴포넌트 구현
    
    * fix: input 컴포넌트 수정
    
    ---------
    
    Co-authored-by: 2nh2oo <p03je8884@gmail.com>
    Co-authored-by: twossu <nadao202@naver.com>

---
+++ src/shared/components/input/input.css.ts
@@ -0,0 +1,55 @@
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+import { themeVars } from '@shared/styles/theme.css';
+
+export const inputVariants = recipe({
+  base: {
+    width: '100%',
+    height: '6rem',
+    padding: '1.3rem 1.2rem',
+    border: `2px solid ${themeVars.color.primary300}`,
+    backgroundColor: themeVars.color.primary100,
+    borderRadius: '10px',
+    outline: 'none',
+    fontFamily: 'inherit',
+
+    ...themeVars.fontStyles.head_eb_18,
+
+    selectors: {
+      '&::placeholder': { color: themeVars.color.brown100 },
+      '&:focus': {
+        borderColor: themeVars.color.primary500,
+      },
+
+      '&:focus::placeholder': { color: 'transparent' },
+    },
+  },
+
+  variants: {
+    hasError: { 
+      false: {}, 
+      
+      true: { 
+        selectors: {
+          '&:not(:placeholder-shown)': {
+            color: themeVars.color.brown200,
+            borderColor: themeVars.color.brown200,
+          },
+          '&:focus::placeholder': {
+            color: 'transparent',
+          },
+        },
+      }, 
+    },
+  },
+
+  defaultVariants: {
+    hasError: false, 
+  },
+});
+
+export const inputContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+});
---
+++ src/shared/components/input/input.tsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import type { ChangeEvent } from 'react';
+
+import * as styles from './input.css';
+
+interface InputProps {
+  value: string;
+  errorState?: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
+
+const Input = ({ value, errorState, onChange, placeholder }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleContainer = () => {
+    inputRef.current?.focus();
+  };
+
+  return (
+    <input
+      ref={inputRef}
+      nterKeyHint="done" 
+      value={value}
+      className={styles.inputVariants({ hasError: !!errorState })}
+      onChange={onChange}
+      placeholder={placeholder}
+      />
+  );
+};
+
+export default Input;
