9a5c395018c35157e882fa592350761d235df94a
2nh2oo <p03jeun@gmail.com>
2026-05-22 04:57:03 +0900

    feat: 메인 홈 화면 UI 구현 (#24)
    * feat: 메인 홈 화면 UI 구현
    
    * fix: 배경 변경
    
    ---------
    
    Co-authored-by: 2nh2oo <p03je8884@gmail.com>
    Co-authored-by: twossu <nadao202@naver.com>

--- src/pages/home/home.css.ts
+++ src/pages/home/home.css.ts
@@ -1,11 +1,19 @@
-import { themeVars } from '@shared/styles/theme.css'
 import { style } from '@vanilla-extract/css'
+import homeBg from '@shared/assets/background-img/home.webp'
 
-export const bgcolor = style({
-  backgroundColor: themeVars.color.brown100,
+export const bgContainer = style({
+  position: 'relative',
+  width: '100vw',
+  height: '100dvh',
+  backgroundImage: `url(${homeBg})`,
+  backgroundSize: 'contain',
+  backgroundRepeat: 'no-repeat',
 })
 
-export const fontstyle = style({
-  ...themeVars.fontStyles.body_60eb,
-  color: themeVars.color.primary300,
+export const button = style({
+  position: 'absolute',
+  width: '25rem',
+  height: '12rem',
+  bottom: '3rem',
+  left: '2rem',
 })
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+import * as styles from './home.css'
+
 const HomePage = () => {
-  return <></>
+  const navigate = useNavigate()
+
+  const handleStartStoryClick = () => {
+    navigate('/agreement')
+  }
+
+  return (
+    <div className={styles.bgContainer}>
+      <button className={styles.button} onClick={handleStartStoryClick} />
+    </div>
+  )
 }
 
 export default HomePage
