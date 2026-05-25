6fc437bbad4cb34eb984673009effd20305c4e2f
twossu <nadao202@naver.com>
2026-05-26 02:51:25 +0900

    fix: 스토리페이지 기분이 어땟어 라벨 추가

--- src/pages/story/story.tsx
+++ src/pages/story/story.tsx
@@ -74,7 +74,17 @@ const StoryPage = () => {
         state: { playSessionId: state.playSessionId, lastNodeId: currentNodeId },
       })
     }
-  }, [isFetching, nodeData, endSession, navigate, state?.playSessionId, state?.selectedChoices, state?.emotionLabel, currentNodeId, createStoryResult])
+  }, [
+    isFetching,
+    nodeData,
+    endSession,
+    navigate,
+    state?.playSessionId,
+    state?.selectedChoices,
+    state?.emotionLabel,
+    currentNodeId,
+    createStoryResult,
+  ])
 
   const handlePrev = () => navigate(-1)
 
@@ -100,6 +110,7 @@ const StoryPage = () => {
               text: content,
             })) ?? []
           }
+          label='기분이 어땠어?'
           onChoiceSelect={handleChoiceSelect}
           disabled={isImageLoading}
         />
